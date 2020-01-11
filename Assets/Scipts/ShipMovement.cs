94c10393e48d3b68d8c205c02cdfe397c9efc53c
YogurtTheHorse <yegorf1@gmail.com>
2020-01-11 17:05:17 +0300

    Implement basing movement

--- Assets/Scipts/ShipMovement.cs
+++ Assets/Scipts/ShipMovement.cs
@@ -1,22 +1,33 @@
 using System;
 using Scipts.Input;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Scipts
 {
-    [RequireComponent (typeof(IPlayerInput))] 
-    [RequireComponent (typeof(Rigidbody))]
+    [RequireComponent(typeof(IPlayerInput))]
+    [RequireComponent(typeof(Rigidbody))]
     public class ShipMovement : MonoBehaviour
     {
-        private IPlayerInput playerInput;
+        private IPlayerInput _playerInput;
+        private Rigidbody _rigidbody;
+        private Transform _transform;
+
+        public float speed = 20f;
+        public float rotationSpeed = 5f;
 
         public void Start()
         {
-            playerInput = GetComponent<IPlayerInput>();
+            _playerInput = GetComponent<IPlayerInput>();
+            _rigidbody = GetComponent<Rigidbody>();
+            _transform = GetComponent<Transform>();
         }
 
         public void Update()
         {
+            _rigidbody.AddForce(_playerInput.Acceleration * speed * transform.forward);
+
+            _transform.Rotate(0f, rotationSpeed * _playerInput.Rotation * Mathf.Min(_rigidbody.velocity.magnitude, 1f), 0f);
         }
     }
 }
