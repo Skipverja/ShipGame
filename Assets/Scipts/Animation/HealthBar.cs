8ced51a3f9111e041e3621b1ba729c770669a0f6
Orfac <molodeckij.b.2017@fml31.ru>
2020-01-12 17:06:21 +0300

    added assets for setting up healthbar

---
+++ Assets/Scipts/Animation/HealthBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image bar;
+    public float fill {get; set;}
+    // Start is called before the first frame update
+    void Start()
+    {
+        fill = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fill -= Time.deltaTime * 0.1f;
+        bar.fillAmount = fill;
+    }
+}
