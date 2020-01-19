0fdcbc514ec07cb6bc1693f0ad3a6ad1adb09894
YogurtTheHorse <yegorf1@gmail.com>
2020-01-19 19:35:52 +0300

    Add EntityCollisionHandler

---
+++ Assets/Scipts/Core/EntityCollisionHandler.cs
@@ -0,0 +1,14 @@
+﻿using Scipts.Core;
+using UnityEngine;
+
+public class EntityCollisionHandler : MonoBehaviour
+{
+    public EntityStats entityStats;
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if (!other.collider.CompareTag(Tags.EntityTag)) return;
+
+        entityStats.Hit(other.relativeVelocity.magnitude);
+    }
+}
