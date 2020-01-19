1b99b98d18258f7cf07308c41ef80e49691bf70b
YogurtTheHorse <yegorf1@gmail.com>
2020-01-19 18:34:28 +0300

    Refactor health bar

--- Assets/Scipts/Animation/HealthBar.cs
+++ Assets/Scipts/Animation/HealthBar.cs
@@ -1,22 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
     public Image bar;
-    public float fill {get; set;}
-    // Start is called before the first frame update
-    void Start()
-    {
-        fill = 1f;
-    }
 
-    // Update is called once per frame
-    void Update()
+    public EntityStats entityStats;
+
+    public void Update()
     {
-        fill -= Time.deltaTime * 0.1f;
-        bar.fillAmount = fill;
+        bar.fillAmount = entityStats.health / entityStats.maxHealth;
     }
 }
