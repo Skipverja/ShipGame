92f88f85e326900e5e678903072e529ad8ead81a
YogurtTheHorse <yegorf1@gmail.com>
2020-01-19 19:50:32 +0300

    Add simple AI input

---
+++ Assets/Scipts/Input/SimpleAiInput.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Scipts.Input;
+using UnityEngine;
+
+public class SimpleAiInput : MonoBehaviour, IPlayerInput
+{
+    public float rotation = 0f;
+    
+    public float acceleration = 0f;
+
+    public float Rotation
+    {
+        get => rotation;
+        set => rotation = value;
+    }
+
+    public float Acceleration
+    {
+        get => acceleration;
+        set => acceleration = value;
+    }
+}
