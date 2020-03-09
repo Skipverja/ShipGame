0dae156ef0ffc817db56f62396cc8f7d2630775c
Orfac <molodeckij.b.2017@fml31.ru>
2020-03-09 16:42:29 +0300

    add basic island with shallow water area

---
+++ Assets/Scipts/Core/ShallowWaterCollisionHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Scipts.Core.Ships;
+using UnityEngine;
+
+namespace Scipts.Core
+{
+    public class ShallowWaterCollisionHandler : MonoBehaviour
+    {
+        private List<Collider> colliders;
+        public void Start(){
+            colliders = new List<Collider>();
+        }
+
+        public void FixedUpdate(){
+            colliders.ForEach(
+                (collider) => {
+                    var shipRigidbody = collider.GetComponent<Rigidbody>();
+                    var transform = collider.GetComponent<Transform>();
+                    shipRigidbody.AddForce(-shipRigidbody.velocity.magnitude * transform.forward);
+                }
+            );
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag(Tags.EntityTag)){
+                colliders.Add(other);
+            }   
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag(Tags.EntityTag)){
+                colliders.Remove(other);
+            }
+
+        }
+    }
+}
