24c7ce404dcbc3b28ba44fde5cb5f503befc0b4c
YogurtTheHorse <yegorf1@gmail.com>
2020-03-09 18:55:57 +0300

    Add Plunging cannon. Fix #3

---
+++ Assets/Scipts/Core/Weapons/PlungingCannon.cs
@@ -0,0 +1,30 @@
+using Mirror;
+using Scipts.Core.Ships;
+using UnityEngine;
+
+namespace Scipts.Core.Weapons
+{
+    public class PlungingCannon : MonoBehaviour, IWeapon
+    {
+        public float cooldown = 3f;
+        public float bulletForce = 5f;
+        public float upForce = 20f;
+
+        public GameObject cannonball;
+
+        public float Cooldown => cooldown;
+
+        public void Shoot(GameObject[] emitters)
+        {
+            foreach (GameObject emitter in emitters)
+            {
+                GameObject shell = Instantiate(cannonball, emitter.transform.position, emitters[0].transform.rotation);
+                var shellRigidbody = shell.GetComponent<Rigidbody>();
+
+                shellRigidbody.AddForce(shell.transform.forward * bulletForce + shell.transform.up * upForce);
+
+                NetworkServer.Spawn(shell);
+            }
+        }
+    }
+}
