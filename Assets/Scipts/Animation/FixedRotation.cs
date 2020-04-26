3da25c5601cd547038a17312952e07f4e6297525
Orfac <molodeckij.b.2017@fml31.ru>
2020-04-26 22:45:32 +0300

    fix rotation for health bar and update its position

---
+++ Assets/Scipts/Animation/FixedRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedRotation : MonoBehaviour
+{
+
+    public Quaternion Rotation {get; set;}
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Rotation;
+        
+    }
+}
